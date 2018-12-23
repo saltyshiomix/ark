7dcaf5d574dc3321790c6f35463a3318b13a8c71
Yoshihide Shiono <shiono.yoshihide@gmail.com>
2018-12-23 23:51:53 +0900

    Refactoring: split the local strategy for better readable codes

--- server/services/auth/auth.module.ts
+++ server/services/auth/auth.module.ts
@@ -4,18 +4,18 @@ import { EnvModule } from '../env/env.module'
 import { UsersModule } from '../../entities/users/users.module';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
-import { LocalStrategy } from './strategies/local.strategy';
+import { LocalSignupStrategy, LocalLoginStrategy } from './strategies/local.strategy';
 
 @Module({
   imports: [EnvModule, UsersModule],
   controllers: [AuthController],
-  providers: [AuthService, LocalStrategy],
+  providers: [AuthService, LocalSignupStrategy, LocalLoginStrategy],
   exports: [AuthService]
 })
 export class AuthModule implements NestModule {
   public configure(consumer: MiddlewareConsumer) {
     consumer
-      .apply(authenticate('local', { failureRedirect: '/auth/signup' })).forRoutes({ path: '/auth/signup', method: RequestMethod.POST })
-      .apply(authenticate('local', { failureRedirect: '/auth/login' })).forRoutes({ path: '/auth/login', method: RequestMethod.POST });
+      .apply(authenticate('local-signup', { failureRedirect: '/auth/signup' })).forRoutes({ path: '/auth/signup', method: RequestMethod.POST })
+      .apply(authenticate('local-login', { failureRedirect: '/auth/login' })).forRoutes({ path: '/auth/login', method: RequestMethod.POST });
   }
 }
--- server/services/auth/strategies/local.strategy.ts
+++ server/services/auth/strategies/local.strategy.ts
@@ -6,31 +6,43 @@ import { AuthService } from '../auth.service';
 import { User } from '../../../entities/users/user.entity';
 
 @Injectable()
-export class LocalStrategy extends PassportStrategy(Strategy) {
+export class LocalSignupStrategy extends PassportStrategy(Strategy, 'local-signup') {
   constructor(private readonly service: AuthService) {
     super({
       usernameField: 'email',
       passwordField: 'password',
       passReqToCallback: true
-    }, async (req, email, password, cb) => {
-      if (req.url === '/auth/signup') {
-        const name: string = req.body.name;
-        password = await hash(password, 8);
-        const user: User = await this.service.createUserIfNotExist({ name, email, password });
-        if (user) {
-          return cb(null, user);
-        }
-        return cb(null, false);
-      } else {
-        let user: User = await this.service.validateUser(email);
-        if (!user) {
-          return cb(null, false);
-        }
-        if (!await compare(password, user.password)) {
-          return cb(null, false);
-        }
-        return cb(null, user);
-      }
     });
+  }
+
+  public async validate(req, email, password) {
+    const { name } = req.body;
+    password = await hash(password, 8);
+    const user: User = await this.service.createUserIfNotExist({ name, email, password });
+    if (user) {
+      return user;
+    }
+    return false;
+  }
+}
+
+@Injectable()
+export class LocalLoginStrategy extends PassportStrategy(Strategy, 'local-login') {
+  constructor(private readonly service: AuthService) {
+    super({
+      usernameField: 'email',
+      passwordField: 'password'
+    });
+  }
+
+  public async validate(email, password) {
+    const user: User = await this.service.validateUser(email);
+    if (!user) {
+      return false;
+    }
+    if (!await compare(password, user.password)) {
+      return false;
+    }
+    return user;
   }
 }
