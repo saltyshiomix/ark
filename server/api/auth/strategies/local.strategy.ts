02e2fb2992a5b042eaf43a447a2cf001990354a7
Yoshihide Shiono <shiono.yoshihide@gmail.com>
2018-12-24 06:08:23 +0900

    Fix broken path to the user entity

--- server/api/auth/strategies/local.strategy.ts
+++ server/api/auth/strategies/local.strategy.ts
@@ -3,7 +3,7 @@ import { Injectable } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import { Strategy } from 'passport-local';
 import { AuthService } from '../auth.service';
-import { User } from '../../../entities/users/user.entity';
+import { User } from '../../../entities/user.entity';
 
 @Injectable()
 export class LocalRegisterStrategy extends PassportStrategy(Strategy, 'local-register') {
