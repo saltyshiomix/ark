d9cd64cb5732eeac461924e5231ea03e4f76c72c
Yoshihide Shiono <shiono.yoshihide@gmail.com>
2019-07-09 02:01:55 +0900

    Fix warning of "Each child in a list should have a unique "key" prop"

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -36,7 +36,7 @@ MyDocument.getInitialProps = async ctx => {
   return {
     ...initialProps,
     styles: [
-      <React.Fragment>
+      <React.Fragment key={0}>
         {initialProps.styles}
         {sheets.getStyleElement()}
       </React.Fragment>
