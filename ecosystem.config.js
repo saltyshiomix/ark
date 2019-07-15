452c7c9b52c086baa7fd66aa7732cf6f76421135
Stanislav V Vyaliy (WiseKaa) <wisekaa03@gmail.com>
2019-07-15 16:50:03 +0300

    .prettierignore

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,13 +1,17 @@
+/** @format */
+
 module.exports = {
-  apps : [{
-    name: 'ARK',
-    script: '.next/production-server/main.js',
-    instances: 1,
-    autorestart: true,
-    watch: false,
-    max_memory_restart: '1G',
-    env: {
-      NODE_ENV: 'production'
-    }
-  }]
+  apps: [
+    {
+      name: 'ARK',
+      script: '.next/production-server/main.js',
+      instances: 1,
+      autorestart: true,
+      watch: false,
+      max_memory_restart: '1G',
+      env: {
+        NODE_ENV: 'production',
+      },
+    },
+  ],
 };
--- server/users/dto/create-user.dto.ts
+++ server/users/dto/create-user.dto.ts
@@ -2,6 +2,8 @@
 
 export class CreateUserDto {
   readonly name: string;
+
   readonly email: string;
+
   readonly password: string;
 }
