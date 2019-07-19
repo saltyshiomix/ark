a8ac509543afa6a01dd93126850daceddb88a48e
Stanislav V Vyaliy (WiseKaa) <wisekaa03@gmail.com>
2019-07-19 14:25:57 +0300

    config service: joi intergration

--- server/config/config.service.ts
+++ server/config/config.service.ts
@@ -2,14 +2,65 @@
 
 import dotenv from 'dotenv';
 import fs from 'fs';
+import * as Joi from 'joi';
+
+export interface EnvConfig {
+  [key: string]: string;
+}
 
 export class ConfigService {
-  private readonly envConfig: { [key: string]: string };
+  private readonly envConfig: EnvConfig;
 
   constructor(filePath: string) {
-    this.envConfig = dotenv.parse(fs.readFileSync(filePath));
-    // eslint-disable-next-line no-debugger
-    // debugger;
+    const config = dotenv.parse(fs.readFileSync(filePath));
+    this.envConfig = this.validateInput(config);
+  }
+
+  /**
+   * Ensures all needed variables are set, and returns the validated JavaScript object
+   * including the applied default values.
+   */
+  private validateInput(envConfig: EnvConfig): EnvConfig {
+    const envVarsSchema: Joi.ObjectSchema = Joi.object({
+      NODE_ENV: Joi.string()
+        .valid(['development', 'production', 'test', 'provision'])
+        .default('development'),
+      PORT: Joi.number()
+        .default(4000)
+        .required(),
+      HOST: Joi.string()
+        .default('http://localhost')
+        .required(),
+      DATABASE_URL: Joi.string()
+        .default('postgres://arkuser:arkark@localhost:5432/arkdb')
+        .required(),
+      DATABASE_SCHEMA: Joi.string()
+        .default('app_public,app_private')
+        .required(),
+      REDIS_HOST: Joi.string()
+        .default('localhost')
+        .required(),
+      REDIS_PORT: Joi.number()
+        .default(6379)
+        .required(),
+      REDIS_DB: Joi.number()
+        .default(0)
+        .required(),
+      REDIS_PASSWORD: Joi.string().allow(''),
+      REDIS_PREFIX: Joi.string().allow(''),
+      SESSION_SECRET: Joi.string()
+        .default('ark')
+        .required(),
+    });
+
+    const { error, value: validatedEnvConfig } = Joi.validate(
+      envConfig,
+      envVarsSchema,
+    );
+    if (error) {
+      throw new Error(`Config validation error: ${error.message}`);
+    }
+    return validatedEnvConfig;
   }
 
   get(key: string): string {
