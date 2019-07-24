a1a053326e8081e7e6f62f0035a724c4aec0d7e0
Yoshihide Shiono <shiono.yoshihide@gmail.com>
2019-07-25 03:31:59 +0900

    Refactoring: simplify

--- next.config.js
+++ next.config.js
@@ -1,11 +1,10 @@
-const { join } = require('path');
 const DotenvWebpackPlugin = require('dotenv-webpack');
 
 module.exports = {
   webpack: (config) => {
     config.plugins = [
       ...(config.plugins || []),
-      new DotenvWebpackPlugin({ path: join(__dirname, '.env') }),
+      new DotenvWebpackPlugin(),
     ];
     return config;
   },
