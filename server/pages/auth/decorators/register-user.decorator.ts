fc0d258d3aa630821cb3b6962923f009a27f33f5
Yoshihide Shiono <shiono.yoshihide@gmail.com>
2019-09-17 19:52:30 +0900

    Remove unused files

--- server/pages/auth/decorators/register-user.decorator.ts
+++
@@ -1,7 +0,0 @@
-import { createParamDecorator } from '@nestjs/common';
-import { RegisterUserDto } from '../dto/register-user.dto';
-
-export const RegisterUser = createParamDecorator((_data, req) => {
-  const { name, email, password } = req.body;
-  return { name, email, password } as RegisterUserDto;
-});
