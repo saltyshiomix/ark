a1e32c0040f056b49401f53111207636c79c7371
Yoshihide Shiono <shiono.yoshihide@gmail.com>
2019-09-17 18:15:41 +0900

    Refactoring

--- server/entities/typeorm.service.ts
+++ server/entities/typeorm.service.ts
@@ -19,7 +19,7 @@ export class TypeOrmService implements TypeOrmOptionsFactory {
       username: this.env.get('DB_USERNAME'),
       password: this.env.get('DB_PASSWORD'),
       database: this.env.get('DB_DATABASE'),
-      synchronize: this.env.get('DB_SYNCHRONIZE') === 'true' ? true : false,
+      synchronize: this.env.get('DB_SYNCHRONIZE') === 'true',
       entities: [__dirname + '/*.entity{.ts,.js}'],
     };
   }
