a27a4bae46db810ee417fb2532007d74e676a7ca
Shiono Yoshihide <shiono.yoshihide@gmail.com>
2019-09-20 17:17:03 +0900

    Refactoring: extract strategies :)

---
+++ server/logics/auth/strategies/index.ts
@@ -0,0 +1,7 @@
+import { LocalRegisterStrategy } from './local-register.strategy';
+import { LocalLoginStrategy } from './local-login.strategy';
+
+export {
+  LocalRegisterStrategy,
+  LocalLoginStrategy,
+};
