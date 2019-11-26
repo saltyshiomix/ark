b3794e68021f4dc2a90e0833a5d9add1242c6adf
Shiono Yoshihide <shiono.yoshihide@gmail.com>
2019-11-26 15:22:03 +0900

    Remove comments

--- server/main.ts
+++ server/main.ts
@@ -5,19 +5,14 @@ import { AppModule } from './app.module';
 import { LogicModule } from './logics/logic.module';
 import { NextModule } from './logics/next/next.module';
 
-// enable environment variables
 dotenv.config();
 
 (async () => {
-  // create nest server
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
-  // initialize logics
   app.get(LogicModule).initialize(app);
 
-  // prepare NEXT.js
   app.get(NextModule).prepare().then(() => {
-    // start a server
     app.listen(process.env.APP_PORT, process.env.APP_HOST, () => {
       console.log(`[ ARK ] Ready on ${process.env.APP_PROTOCOL}://${process.env.APP_HOST}:${process.env.APP_PORT}`);
     });
