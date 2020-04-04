f005f5e90da63ee4eb1f97bdbb9c68ccf4af9ae8
Shiono Yoshihide <shiono.yoshihide@gmail.com>
2020-04-04 09:41:23 +0900

    Remove typeface-roboto because it causes network errors

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,3 @@
-// tslint:disable:react-a11y-titles
-
 import React from 'react';
 import App from 'next/app';
 import Head from 'next/head';
@@ -7,25 +5,12 @@ import { ThemeProvider } from '@material-ui/styles';
 import { CssBaseline } from '@material-ui/core';
 import { theme } from '../lib/theme';
 
-require('typeface-roboto');
-
-const FontFaceObserver = require('fontfaceobserver');
-const initializeFonts = () => {
-  const roboto = new FontFaceObserver('Roboto');
-  roboto.load().then(() => {
-    document.documentElement.classList.add('roboto');
-  });
-};
-
 class MyApp extends App {
   componentDidMount() {
     const jssStyles = document.querySelector('#jss-server-side');
     if (jssStyles) {
       jssStyles.parentNode!.removeChild(jssStyles);
     }
-
-    // non-blocking loading fonts
-    initializeFonts();
   }
 
   render() {
